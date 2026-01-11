823c7df3c6108f59215b52bda8070e08b20ae36b
antuabinash <150573436+antuabinash@users.noreply.github.com>
2026-01-11 22:30:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "qbank-v2";
+const CACHE_NAME = "qbank-v3";
 const assets = [
   "./",
   "./index.html",
@@ -9,16 +9,12 @@ const assets = [
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
