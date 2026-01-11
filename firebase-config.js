362ca516cae7772f9858672e0ad7634a57d9edd6
antuabinash <150573436+antuabinash@users.noreply.github.com>
2026-01-11 22:28:23 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
-// REPLACE WITH YOUR KEYS LATER
+// TODO: PASTE YOUR FIREBASE KEYS HERE
 const firebaseConfig = {
     apiKey: "AIzaSyB54YuV-seHAxRhTia0nuuDmE5Sh2ta61w",
   authDomain: "ertquiz.firebaseapp.com",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
